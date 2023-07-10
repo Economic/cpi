e06561ba7ebdb28b13f94876867c55507389795c
jkandra-epi <jkandra@epi.org>
2023-07-10 09:13:13 -0400

    add missing packages

--- code/cpi_master.R
+++ code/cpi_master.R
@@ -7,15 +7,20 @@ library(bea.R)
 library(here)
 library(zoo)
 library(tsibble)
+library(feasts)
 library(seasonal)
 library(stringr)
 
 #BLS registration key needed for api version 2. register here: https://data.bls.gov/registrationEngine/
 bls_key <- Sys.getenv("BLS_REG_KEY")
 
-#year
+# set year object
 current_year <- c(2023)
 
+
+# run script to pull in CPI
+#note: IF BLS HAS NOT RELEASE CPI DATA FOR THE PAST YEAR, RUN THIS SCRIPT
+source("code/cpi_alt.R", echo = TRUE)
 
 # run script to pull in CPI
 #note: IF BLS HAS RELEASED CPI DATA FOR PAST YEAR, RUN THIS SCRIPT
